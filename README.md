f6c27579bf9d8accf00bda7b0845eeae1e8bfe94
Forbes85 <vudo8585@gmail.com>
2023-04-20 23:03:57 -0500

    edited README and js

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Password-Generator
 
+## Making a random password generator
+
+The page was made using a lot of JavaScript and some HTML and CSS. The page was created in order to give a user the option of a secure random generated password that could include a combination of Uppercase letters, Lowercase letters, Numbers,  and Symbols. The user can also choose the length of the password but only if the number they choose is between 8 and 128 characters in length. The cool thing about the generator is the user has the option to choose exactly which characters they want in their password and which characters they don't. After the user selects the length, which charcters they want to include, and verify they accept the criteria a password is generated in the text box to copy and us when needed. This is purely for add security when logging into one of your favorite sites.
+
 ## Installation
 
 I used Visual Studio Code (VScode) as my code editor it's easy to use and have many extensions to make your coding experience go smoother, but of course use any code editor you are comfortable with you can access and download the program for Windows or Mac at the following link: https://code.visualstudio.com/download
@@ -42,3 +46,17 @@ I used a GitHub.com as my main repository database in order to save and re-edit 
 - If our branches are up to date we procede to merging them into our main branch by typing 'git push' just to push all or 'git push origin feature/' command to select a certain branch: ![Screenshot push all](https://user-images.githubusercontent.com/124540000/229528348-ad6663c1-3d36-4fa8-9e58-db3217cbbdb2.png) or ![Screenshot git push feature](https://user-images.githubusercontent.com/124540000/229528688-5e40d5fb-fe47-4537-bfa7-3a824ef3c1be.png)
 
 With these steps you should now be able to clone a repository, edit the contents, and save your changes to others can see and even edit your changes. With this being my personal portfolio page if you do open it up through https://github.com/Forbes85/My-Portfolio- PLEASE only leave comments for changes so that I can review and possibly implement those changes.
+
+## Screenshots
+
+![password generator](https://user-images.githubusercontent.com/124540000/233536437-7d86d0b9-6481-4bf2-8b2c-e176f2900c81.png) "Password Generator page"
+
+![passlength](https://user-images.githubusercontent.com/124540000/233536617-e604762e-65bb-4fb4-bded-16252b69d00c.png) "Password length prompt window"
+
+![upper](https://user-images.githubusercontent.com/124540000/ 233536715-90451e2a-9caa-4d47-a6d5-c3563660b4fa.png) "Password uppercase confirm window"
+
+![lower](https://user-images.githubusercontent.com/124540000/233536801-68e7c66e-a2bd-44b5-a49c-4f266cc3c109.png) "Password lowercase confirm window"
+
+![symbol](https://user-images.githubusercontent.com/124540000/233536915-50f23f74-ff03-449b-b4fd-d02939208318.png) "Password symbol confirm window"
+
+![number](https://user-images.githubusercontent.com/124540000/233537199-ea5e544e-84bf-4194-bae5-c358ed23b3fe.png) "Password number confirm window"
--- script.js
+++ script.js
@@ -2,79 +2,93 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
-
-
+var chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
 
 // Write password to the #password input
 function generatePassword() {
- var password = [];
- const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
- const symbol = ["! @ # $ % ^ & * ( )"];
- const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
- const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var combinedChars = upper.concat(lower, symbol, number);
+  var eligibleCharacters = [];
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const symbol = "!@#$%^&*() ";
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const number = '0123456789';
+  //var combinedChars = upper.concat(lower, symbol, number);
+
+  var passLength = window.prompt("Choose your password length");
+
+  // passLength = Number(passLength)
   
-
-
-   var passLength = window.prompt('Choose your password length')
   if (passLength < 8) {
-    alert('Password must be at least 8 characters!') 
+    alert("Password must be at least 8 characters!");
     return;
+  
   }
+  // todo make sure number entered was a valid number
 
-    
   if (passLength > 128) {
-    alert('Password cannot be greater than 128 characters!') 
+    alert("Password cannot be greater than 128 characters!");
     return;
-    
+  }
+  //   if (isNaN){
+  //   alert("Please choose only numbers!!!")
+  //   return;
+  // }
+
+  var uppercase = window.confirm(
+    "Do you want uppercase letters? Click OK for Yes and cancel for NO"
+  );
+  if (uppercase === true) {
+    eligibleCharacters = eligibleCharacters.concat(upper);
   }
 
-  
-   var uppercase = window.confirm("Do you want uppercase letters? Click OK for Yes and cancel for NO")
-     if (uppercase === true){
-      password = password.concat(upper);
-   }
-
-
-  var lowercase = window.confirm("Do you want lowercase letters? Click OK for Yes and cancel for NO")
-  if (lowercase === true){
-    password = password.concat(lower);
- }
- 
-  var numbers = window.confirm("Do you want to add numbers? Click OK for Yes and cancel for NO")
-  if (numbers === true){
-    password = password.concat(number);
- }
- 
-  var symbols = window.confirm("Do you want to add symbols? Click OK for Yes and cancel for NO")
-  if (symbol === true){
-    password = password.concat(symbol);
- }
-    
-    for (var i = 0; i <= passLength; i++) {
-      var randomNumber = Math.floor(Math.random() * password.length);
-      password += chars.substring(randomNumber, randomNumber);
-    }
+  var lowercase = window.confirm(
+    "Do you want lowercase letters? Click OK for Yes and cancel for NO"
+  );
+  if (lowercase === true) {
+    eligibleCharacters = eligibleCharacters.concat(lower);
   }
- 
 
-  
+  var numbers = window.confirm(
+    "Do you want to add numbers? Click OK for Yes and cancel for NO"
+  );
+  if (numbers === true) {
+    eligibleCharacters = eligibleCharacters.concat(number);
+  }
+
+  var symbols = window.confirm(
+    "Do you want to add symbols? Click OK for Yes and cancel for NO"
+  );
+  if (symbol === true) {
+    eligibleCharacters = eligibleCharacters.concat(symbol);
+  }
+  //todo check for at least one option
+
+  var password = document.getElementById('password');
+
+  for (var i = 0; i <= passLength; i++) {
+    var randomNumber = Math.floor(Math.random() * eligibleCharacters.length);
+    password += eligibleCharacters.length(randomNumber + 1);
+  }
+  document.getElementById('password').value = password;
+// return the password that is generated
+
+}
+
 function toDelete() {
+  password = [];
   var passwordText = document.querySelector("#password");
- const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
- const symbol = ["! @ # $ % ^ & * ( )"];
- const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
- const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const symbol = "!@#$%^&*() ";
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const number = '0123456789';
   var result = upper.concat(lower, symbol, number);
 
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber);
   }
-  
-  
-  passwordText.value = (password);
+
+  passwordText.value = password;
 }
 
 function writePassword() {
@@ -82,14 +96,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
-
-
 
 // step 1 when the button is clicked a prompt will meet to user.-prompts can be found in the mini project.
 
@@ -99,4 +110,4 @@ generateBtn.addEventListener("click", writePassword);
 
 // step 4 define a var for the user to choose the length of the password.
 
-// create a boolean prompt for each array 
+// create a boolean prompt for each array
